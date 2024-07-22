780cff8a09514374c77f98d589abf32f40d6a91b
Yadnyesh-Dashpute <115160579+Yadnyesh-Dashpute@users.noreply.github.com>
2024-07-22 20:01:07 +0530

    Create user.js

---
+++ Week 4/4.3 Assignment/04-mongo-with-jwt-auth/middleware/user.js
@@ -0,0 +1,24 @@
+const {JWT_SECRET} = require("../config")
+const jwt = require("jsonwebtoken")
+
+function userMiddleware(req, res, next) {
+    // Implement user auth logic
+    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    
+    const token = req.headers.authorization
+    const words = token.split(" ")
+    const jwtToken = words[1]
+
+    const decode = jwt.verify(jwtToken, JWT_SECRET)
+    if(decode.username){
+        req.username = decode.username
+        next()
+    }else{
+        res.status(403).json({
+            msg: "You are not Authenticate"
+        })
+    }
+
+}
+
+module.exports = userMiddleware;
