64dcbbf264db192a28b5c8fa13820071eb2f103d
Yadnyesh-Dashpute <115160579+Yadnyesh-Dashpute@users.noreply.github.com>
2024-07-25 01:57:28 +0530

    Add files via upload

---
+++ Week 5 /Lecture 5.2/frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,48 @@
+import { title } from "process"
+import { useState } from "react"
+
+
+
+
+export function CreateTodo(props) {
+
+
+    const [title, setTitle] = useState("");
+    const [description, setDescription] = useState("");
+
+
+    return <div>
+
+        <input style ={{
+            padding:10,
+            margin:10
+        }} type="text" placeholder="title" onChange={function(e){
+            const value = e.target.value;
+            setTitle(e.target.value);
+        }}></input><br></br>
+        <input style ={{
+            padding:10,
+            margin:10
+        }}type="text" placeholder="description" onChange={function(e){
+            const value = e.target.value;
+            setDescription(e.target.value)
+        }}></input><br></br>
+        <button onClick={ () => {
+            fetch("http://localhost:3000/todo"), {
+                method: "POST",
+                body: JSON.stringify( {
+                    title: title,
+                    description: description
+                }),
+                headers: {
+                    "Content-type": "application/json"
+                }
+            }
+            .then(async function(res) {
+                const json = await res.json()
+                alert("Todo Added ")
+            })
+        }}>Add a Todo</button>
+    </div>
+}
+
