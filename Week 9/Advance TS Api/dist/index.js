3e3b5992c752f590ba6f300e007439cce523959d
Yadnyesh-Dashpute <115160579+Yadnyesh-Dashpute@users.noreply.github.com>
2024-10-23 16:38:31 +0530

    Delete Week 9/Advance TS Api directory

--- Week 9/Advance TS Api/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// ---- 1. Interface In TS:
-const displayUserProfile = (user) => {
-    console.log(`Name: ${user.name}, Email: ${user.email}`);
-};
-displayUserProfile({ name: "Rohan" });
